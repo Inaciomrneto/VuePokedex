454c2e7516c85e7977c6404aafa8db2fa5b98229
Inácio <inacio@fourup.com>
2022-03-21 13:55:40 -0300

    Pokedex Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,160 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- lista e campo de busca -->
+  <v-app>
+    <v-container>
+      <v-container>
+        <v-row>
+          <v-container>
+            <h1 class="text-center white--text mb-8" style="font-size= 4rem">Pokedex 1ª geração</h1>
+          </v-container>
+        </v-row>
+      </v-container>
+      <v-container>
+        <v-card>
+          <v-text-field v-model="search" placeholder="Digite o nome do pokémon" class="input"></v-text-field>
+        </v-card>
+        <v-row class="space">
+          <v-col cols="3" v-for="pokemon in searchPokemons" :key="pokemon.id">
+            <v-card @click="showPokemon(getId(pokemon))">
+              <v-container>
+                <v-row class="mx-0 d-flex justify-center">
+                  <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getId(pokemon)}.png`" :alt="pokemon.name" width="80%">
+                </v-row>
+                <h2 class="text-center">{{`#${getId(pokemon)} ` + getName(pokemon)}}</h2>
+              </v-container>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+    
+    <!-- detalhes --> 
+
+    <v-dialog
+      v-model="showDioalog"
+      width="800"
+    >
+      <v-card v-if="selectedPokemon" class="px-6">
+        <v-container>
+          <v-row class="d-flex align-center">
+            <v-col cols="4">
+              <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png`" :alt="selectedPokemon.name" width="80%">
+            </v-col>
+            <v-col cols="8">
+              <h1>{{getName(selectedPokemon)}}</h1>
+              <LabelTypeColor v-for="type in selectedPokemon.types" :key="type.slot" class="mr-2" :typePoke=type.type.name></LabelTypeColor>
+              <v-divider class="my-4"></v-divider>
+              <v-chip label>Altura: {{ selectedPokemon.height * 2.54 }} cm</v-chip>
+              <v-chip label class="ml-2" >Peso: {{ (selectedPokemon.weight * 0.45359237).toFixed(0) }} Kgs</v-chip>
+            </v-col>
+          </v-row>
+          <h2>Moves</h2>
+          <v-simple-table>
+            <template>
+              <thead>
+                <tr>
+                  <th class="text-left">Nível</th>
+                  <th class="text-left">Nome</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in filterMoves(selectedPokemon)" :key="item.move.name">
+                  <td>{{getMoveLevel(item)}}</td>
+                  <td>{{ item.move.name }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import labelTypeColor from './components/LabelTypeColor.vue';
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    labelTypeColor,
+  },
+
+  data() {
+    return{
+      pokemons: [],
+      search: '',
+      showDioalog: false,
+      selectedPokemon: null,
+    }
+  },
+
+  mounted(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then((response) => { 
+      this.pokemons = response.data.results;
+    })
+  },
+
+  methods: {
+    getId(pokemon){
+      return Number(pokemon.url.split("/")[6]);
+    },
+    getName(pokemon) {
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    },
+    showPokemon(id){
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => { 
+        this.selectedPokemon = response.data;
+        this.showDioalog = !this.showDioalog;
+      })
+    },
+    getMoveLevel(move){
+      for(let version of move.version_group_details){
+        if(version.version_group.name == "red-blue" && version.move_learn_method.name == "level-up"){
+          return version.level_learned_at;
+        }
+      }
+      return 0;
+    },
+    filterMoves(pokemon){
+      return pokemon.moves.filter((item) => {
+        let include = false;
+        for(let version of item.version_group_details){
+          if(version.version_group.name == "red-blue" && version.move_learn_method.name == "level-up"){
+            include = true;
+          }
+        } 
+        return include;
+      })
+    }
+  },
+
+  computed: {
+    searchPokemons(){
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      })
+    },
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    background: linear-gradient(to bottom right,rgba(12,39,63,1), rgb(47, 107, 160)) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    background-position: center;
+    min-height: 100vh;
+    }
+    .input{
+      margin: 30px;
+    }
+    .space{
+      padding-top: 15px;
+    }
 </style>
---
+++ src/components/LabelTypeColor.vue
@@ -0,0 +1,76 @@
+<template>  
+  <v-chip :color=getColor(typePoke)>{{typePoke.charAt(0).toUpperCase() + typePoke.slice(1)}}</v-chip>
+</template>
+
+<script>  
+
+export default {
+  name: 'LabelTypeColor',
+
+  components: {
+  },
+
+  props:{
+    typePoke: String,
+  },
+
+methods:{
+  getColor(type){
+    if(type === 'normal'){
+      return '#b3b3a8';
+    }
+    else if(type === 'poison'){
+      return '#a85e9a';
+    }
+    else if(type === 'psychic'){
+      return '#ea62a8';
+    }
+    else if(type === 'grass'){
+      return '#82c963';
+    }
+    else if(type === 'ground'){
+      return '#dbbf6b';
+    }
+    else if(type === 'ice'){
+      return '#87dffb';
+    }
+    else if(type === 'fire'){
+      return '#ee524b';
+    }
+    else if(type === 'rock'){
+      return '#c1b176';
+    }
+    else if(type === 'dragon'){
+      return '#8577f5';
+    }
+    else if(type === 'water'){
+      return '#50a6f9';
+    }
+    else if(type === 'bug'){
+      return '#b7c54c';
+    }
+    else if(type === 'dark'){
+      return '#8b6958';
+    }
+    else if(type === 'fighting'){
+      return '#ba5e55';
+    }
+    else if(type === 'ghost'){
+      return '#7675c1';
+    }
+    else if(type === 'steel'){
+      return '#b8b7c9';
+    }
+    else if(type === 'flying'){
+      return '#719cee';
+    }
+    else if(type === 'electric'){
+      return '#f3d55e';
+    }
+    else if(type === 'fairy'){
+      return '#e7a5e7';
+    }
+  },
+},
+};
+</script>
